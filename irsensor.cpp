84b45f545c96478f02c446f1502797239a205d22
Vinai Birbal <vinaib9@hotmail.com>
2024-07-14 00:18:23 -0400

    Update irsensor.cpp

--- irsensor.cpp
+++ irsensor.cpp
@@ -2,22 +2,36 @@
 #include "hardware.h"
 #include "irsensor.h"
 
+
 void IR::setPin(int pin){
-  pinMode(pin, INPUT);
-  this->pin = pin;
-  this->edgeFound = false;
+    pinMode(pin, INPUT);
+    this->pin = pin;
+}
+void IR::readIrSensor(){
+
+    Serial.begin(9600);
+
+    if(digitalRead(pin) == LOW){
+       this->trig = true;
+    }
+    else{
+        this->trig = false;
+    }
+
+}
+bool IR::getIrTrig(int){
+
+    readIrSensor();
+    return this->trig;
+
+}
+void IR::printIrTrig(){
+    if(ir_sensors){
+        Serial.println("edge detected");
+    }
+    else{
+        Serial.println("edge not detected");
+    }
 }
 
-bool IR::readIrSensor(){
 
-  if(digitalRead(this->pin) == LOW){
-    this->edgeFound = true;
-    Serial.println("Edge detected");
-  }
-  else{
-    this->edgeFound = false;
-    Serial.println("Edge not detected");
-  }
-
-  return this->edgeFound;
-}
