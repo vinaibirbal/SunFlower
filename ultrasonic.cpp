3e9137b93f85213c1776584651bed20094d821cb
Vinai Birbal <vinaib9@hotmail.com>
2024-07-14 00:23:00 -0400

    Update ultrasonic.cpp

--- ultrasonic.cpp
+++ ultrasonic.cpp
@@ -1,44 +1,52 @@
 #include "Arduino.h"
+#include "hardware.h"
 #include "ultrasonic.h"
 
-const int trig_threshold = 5;
-
 void UltraSonic::setPins(int trig, int echo) {
+    this->trigPin = trig;
+    this->echoPin = echo;
+}
+void UltraSonic::setup(int trig, int echo) {
+  
   this->trigPin = trig;
   this->echoPin = echo;
-  this->objectFound = false;
-  pinMode(this->trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(this->echoPin, INPUT); // Sets the echoPin as an Input
+  this->trig = 5;
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
 }
+void UltraSonic::readUltraSonic(){
 
-bool UltraSonic::readUltraSonic(){
-
-  // Clears the trigPin
+  setup();
+    // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  
   // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  float duration = pulseIn(echoPin, HIGH);
-  
+  duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  this->distance = duration * 0.034 / 2;
-  int obj_distance = this->distance;
-  Serial.println(this->distance);
-  
-  if(obj_distance < trig_threshold){
-    this->objectFound = true;
-    Serial.println("Found an object. I should stop moving or not move");
+  distance = duration * 0.034 / 2;
+  this->distance = distance;
+  if(distance > this->trig_dist){
+    trig = true;
   }
-  else{
-    this->objectFound = false;
-    Serial.println("Object not in range");
-  }
-
-  return this->objectFound;
 
 }
+int UltraSonic::getUltraSonic(){
+    readUltraSonic();
+    return this->distance;
+
+}
+void UltraSonic::setTrigLevel(int trig_dist){
+    this->trigLevel = trig_dist;
+
+}
+void UltraSonic::printDistance(){
+
+    Serial.println(this->distance);
+
+}
+
